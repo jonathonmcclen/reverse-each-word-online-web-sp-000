f2d70613ad202dfe38ef8af484b37668a61bc326
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-29 18:03:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_string = ""
   array.collect do |word|
-    new_string << word.reverse
+    new_array = new_array.push() word.reverse
   end
   new_string.join(" ")
 end
